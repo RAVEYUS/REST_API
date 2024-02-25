e732cfcf3c6bdf3a0d677c1edcbd96d04cc077e8
RAVEYUS <gtavcs35@gmail.com>
2024-02-25 19:10:43 +0530

    Added a new endpoint which fetches JSON Data by Id

--- main.py
+++ main.py
@@ -23,18 +23,27 @@ class UserSchema(BaseModel):
     class Config:
         orm_mode=True
 
-@app.get("/users", response_model=list[UserSchema])
+@app.get("/users", response_model=list[UserSchema], summary="fetch the list of all tasks")
 def get(db:Session=Depends(get_db)):
     return db.query(models.users).all()
 
-@app.post("/users", response_model=UserSchema)
+@app.get("/users/{user_id}", response_model=list[UserSchema], summary="fetch a particular task by their id")
+def get(user_id: int, db:Session=Depends(get_db)):
+
+    try:
+        u = db.query(models.users).filter(models.users.id == user_id).first()
+        return u
+    except:
+        raise HTTPException(status_code=404, detail="User not found")
+
+@app.post("/users", response_model=UserSchema,summary= "Add a new task")
 def post(user:UserSchema,db:Session=Depends(get_db)):
     u=models.users(Header=user.Header,Body=user.Body)
     db.add(u)
     db.commit()
     return u
 
-@app.put("/users/{user_id}", response_model=UserSchema)
+@app.put("/users/{user_id}", response_model=UserSchema,summary= "Update an existing task")
 def update_user(user_id: int, user: UserSchema, db: Session = Depends(get_db)):
     try:
         u = db.query(models.users).filter(models.users.id == user_id).first()
@@ -49,7 +58,7 @@ def update_user(user_id: int, user: UserSchema, db: Session = Depends(get_db)):
 class DeleteResponse(BaseModel):
     message: str
 
-@app.delete("/users/{user_id}", response_model=DeleteResponse)
+@app.delete("/users/{user_id}", response_model=DeleteResponse, summary="Delete a user by id")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
 
     try:
